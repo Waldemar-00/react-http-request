a5f1df4307a8987167c3cac1248cd81723d5f036
uladzimir <01myit@gmail.com>
2023-10-15 15:59:05 +0300

    fetching from Joke API

--- src/App.js
+++ src/App.js
@@ -1,28 +1,20 @@
-import React from "react" 
+import React, { useState } from "react" 
 
 import JokeList from "./components/JokeList" 
 import "./App.css" 
 
 function App() {
-  const dummyJokes = [
-    {
-      id: 1,
-      type: "general",
-      setup: "What do you call a bee that lives in America?",
-      punchline: "A USB.",
-    },
-    {
-      id: 2,
-      type: "programming",
-      setup: "What's the best thing about a Boolean?",
-      punchline: "Even if you're wrong, you're only off by a bit.",
-    },
-  ] 
-
+  const [dummyJokes, setDummyJokes] = useState([])
+  function fetchJokesHandler() {
+    fetch('https://official-joke-api.appspot.com/random_ten')
+      .then(response => response.json()).then(data => setDummyJokes(data))
+  }
   return (
     <React.Fragment>
       <section>
-        <button>Fetch Jokes</button>
+        <button
+          onClick={fetchJokesHandler}
+        >Fetch Jokes</button>
       </section>
       <section>
         <JokeList jokes={dummyJokes} />
