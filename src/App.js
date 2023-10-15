194ec77cbc6f5f6e296a46c69770b3ea56e30d64
uladzimir <01myit@gmail.com>
2023-10-15 17:50:43 +0300

    added message about loading

--- src/App.js
+++ src/App.js
@@ -4,11 +4,14 @@ import JokeList from "./components/JokeList"
 import "./App.css" 
 
 function App() {
-  const [dummyJokes, setDummyJokes] = useState([])
+  const [dummyJokes, setDummyJokes] = useState(['0'])
+  const [isDummy, setIsDummy] = useState(false)
   async function fetchJokesHandler() {
+    setIsDummy(true)
     const response = await fetch('https://official-joke-api.appspot.com/random_ten')
     const data = await response.json()
     setDummyJokes(data)
+    setIsDummy(false)
   }
   return (
     <React.Fragment>
@@ -17,9 +20,14 @@ function App() {
           onClick={fetchJokesHandler}
         >Fetch Jokes</button>
       </section>
-      <section>
-        <JokeList jokes={dummyJokes} />
-      </section>
+      {
+        dummyJokes[0] === '0' && !isDummy ? null :
+          <section>
+            {
+              isDummy ? < JokeList jokes={[{ setup: 'Geting Jokes from GitHub...' }]} /> : < JokeList jokes={dummyJokes} />
+            }
+          </section>
+      }
     </React.Fragment>
   ) 
 }
--- src/components/JokeList.js
+++ src/components/JokeList.js
@@ -3,18 +3,18 @@ import React from "react";
 import Joke from "./Joke";
 import styles from "./JokeList.module.css";
 
-const JokeList = (props) => {
+const JokeList = ({ jokes }) => {
   return (
-    <ul className={styles["joke-list"]}>
-      {props.jokes.map((joke) => (
-        <Joke
-          key={joke.id}
-          type={joke.type}
-          setup={joke.setup}
-          punchline={joke.punchline}
-        />
-      ))}
-    </ul>
+      <ul className={styles["joke-list"]}>
+        {jokes.map((joke) => (
+          <Joke
+            key={joke.id}
+            type={joke.type}
+            setup={joke.setup}
+            punchline={joke.punchline}
+          />
+        ))}
+      </ul >
   );
 };
 
